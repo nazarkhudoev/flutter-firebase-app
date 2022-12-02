48ffccd80a798f5e352593094bb905fc39a523a6
unknown <shukhrat.nazarkhudoev@gmail.com>
2022-12-02 10:23:51 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/login_view.dart';
+
+import 'firebase_options.dart';
+
+String getFullName(String firstName, String lastName) {
+  return '$firstName $lastName';
+}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You are verified user');
+              } else {
+                print('You need to verified your email first');
+              }
+              return const Text('Done');
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
